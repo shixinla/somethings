c3ada6b94f5b5c7174a540e43cdb4204f7597b69
shixinla <shixin.work@outlook.com>
2021-06-17 00:42:13 +0800

    mutiple build config and test

---
+++ src/pages/promise/promise.ts
@@ -0,0 +1,1 @@
+let schedule = 1
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        promise: resolve(__dirname, 'examples/promise/index.html'),
+        taskScheduler: resolve(__dirname, 'examples/task-scheduler/index.html'),
+        test: resolve(__dirname, 'src/index.html')
+      }
+    }
+  },
   plugins: [vue()]
 })
